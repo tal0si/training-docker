c8a9482b77035f56a3718359b6cf3b1b2010c6f8
Nicolas Deblock <nico.deblock@gmail.com>
2022-10-15 16:21:41 +0200

    ✨ dockerfile compose

--- bdd/init-db.sql
+++ bdd/init-db.sql
@@ -5,3 +5,5 @@ USE workshop;
 CREATE TABLE USERS (id INT PRIMARY KEY NOT NULL,name VARCHAR(100),firstname VARCHAR(100));
 
 INSERT INTO USERS VALUES (1, "anne", "onyme");
+
+GRANT ALL ON *.* TO 'root'@'%';
